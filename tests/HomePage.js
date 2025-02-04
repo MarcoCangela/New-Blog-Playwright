1c339093c54fa752e86524ddabcd6bb14a739746
Marco Cangela Garujo <Mcangelaoo@gmail.com>
2025-02-04 19:04:31 +0200

    Setting up and starting the tests

--- tests/HomePage.js
+++ tests/HomePage.js
@@ -1,5 +1,9 @@
 const {test, expect} = require('@playwright/test')
 
-test('',{
-    
+test.beforeAll(({page}) => {
+    page.goto('127.0.0.1:3000');
+})
+
+test('Homepage has title', async ({ page }) => {
+    await expect(page).toHaveTitle(/Home Page/);
 })
