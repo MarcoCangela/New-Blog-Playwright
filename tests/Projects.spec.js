6011613658c14288ff0d3554941ad89eec7b9d96
Marco Cangela Garujo <Mcangelaoo@gmail.com>
2025-02-07 20:33:41 +0200

    Adding Projects page to test

--- tests/Projects.spec.js
+++ tests/Projects.spec.js
@@ -1,7 +1,7 @@
 const {test, expect} = require('@playwright/test')
 
 test.beforeAll(({page}) => {
-    page.goto('127.0.0.1:3000');
+    page.goto('127.0.0.1:3000/projects');
 })
 
 test('Homepage has title', async ({ page }) => {
